4e58bf39c92d103cafa5cece97614e1311306ed0
MC-117 <295566948@qq.com>
2020-11-29 15:32:53 +0800

    Add feature
    object copy & paste support

--- Tangram/EditorWindow.cpp
+++ Tangram/EditorWindow.cpp
@@ -8,13 +8,36 @@ EditorWindow::EditorWindow(Object & object, Material& baseMat, string name, bool
 {
 }
 
+string nextName(const string& name)
+{
+	int num = 0, i = 0;
+	for (; i < name.size(); i++) {
+		char c = name[name.size() - 1 - i];
+		if (isdigit(c))
+			num += (c - '0') * pow(10, i);
+		else
+			break;
+	}
+	if (num == 0 && i == 0)
+		return name + '1';
+	return name.substr(0, name.size() - i) + to_string(num + 1);
+}
+
+string getNextName(const string& name)
+{
+	string newName = nextName(name);
+	while(Brane::find(typeid(Object).hash_code(), newName) != NULL)
+		newName = nextName(newName);
+	return newName;
+}
+
 void EditorWindow::onRenderWindow(GUIRenderInfo& info)
 {
 	World* w = dynamic_cast<World*>(&object);
 	if (w == NULL)
 		return;
-	static World& world = *w;
-	static GUI& gui = info.gui;
+	World& world = *w;
+	GUI& gui = info.gui;
 	ImGui::Text("FPS: %f", ImGui::GetIO().Framerate);
 	ImGui::Separator();
 	ImVec2 size = { ImGui::GetWindowContentRegionWidth() / 2 - 4, 40 };
@@ -117,13 +140,11 @@ void EditorWindow::onRenderWindow(GUIRenderInfo& info)
 		return;
 	if (ImGui::Button("Quit Editor", size))
 		world.quit();
-	static float mainVolume = 1;
 	if (ImGui::SliderFloat("Volume", &mainVolume, 0, 1)) {
 		world.setMainVolume(mainVolume);
 	}
 	ImGui::TextColored(ImVec4(1, 1, 0, 1), "Objects");
 	ImGui::BeginChild("ObjectsView", { -0.1f, -0.1f });
-	static Object* selectedObj = NULL;
 	ImGuiTreeNodeFlags node_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick;
 	if (selectedObj == &world)
 		node_flags |= ImGuiTreeNodeFlags_Selected;
@@ -148,30 +169,87 @@ void EditorWindow::onRenderWindow(GUIRenderInfo& info)
 				world += win;
 			}
 		}
-		world.traverse([](Object& obj)->bool {
-			ImGuiTreeNodeFlags node_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick;
-			if (obj.children.empty())
-				node_flags |= ImGuiTreeNodeFlags_Leaf;
-			if (selectedObj == &obj)
-				node_flags |= ImGuiTreeNodeFlags_Selected;
-			bool isOpen = ImGui::TreeNodeEx(obj.name.c_str(), node_flags);
-			if (ImGui::IsItemClicked()) {
-				selectedObj = &obj;
-				InspectorWindow* ipw = dynamic_cast<InspectorWindow*>(gui.getUIControl("Inspector"));
-				if (ipw == NULL) {
-					ipw = new InspectorWindow(world, "Inspector", true);
-					world += ipw;
-
-				}
-				ipw->setTargetObject(obj);
-				ipw->show = true;
-			}
-			return isOpen;
-		}, [](Object& obj, bool run) {
-			if (run)
-				ImGui::TreePop();
-		});
+		traverse(world, gui);
 		ImGui::TreePop();
 	}
+	
 	ImGui::EndChild();
+}
+
+void EditorWindow::onPostAction(GUIPostInfo & info)
+{
+	World* w = dynamic_cast<World*>(&object);
+	if (w == NULL)
+		return;
+	World& world = *w;
+	if (isFocus() || info.focusControl == NULL) {
+		if (world.input.getKeyDown(VK_CONTROL)) {
+			if (selectedObj != NULL &&
+				world.input.getKeyPress('C')) {
+				Console::log("Ctrl+C");
+				copyInfo.clear();
+				if (selectedObj->serialize(copyInfo)) {
+					parentObj = selectedObj->parent;
+					copyInfo.serialization = SerializationManager::getSerialization(copyInfo.type);
+				}
+				else {
+					parentObj = NULL;
+					copyInfo.clear();
+				}
+			}
+			else if (copyInfo.serialization != NULL &&
+				world.input.getKeyPress('V')) {
+				string oldName = copyInfo.name;
+				string newName = getNextName(copyInfo.name);
+				Console::log("Ctrl+V: %s", newName.c_str());
+				copyInfo.name = newName;
+				Serializable* ser = copyInfo.serialization->instantiate(copyInfo);
+				if (ser != NULL) {
+					Object* obj = dynamic_cast<Object*>(ser);
+					if (obj == NULL)
+						delete ser;
+					else {
+						Object* pobj = parentObj == NULL ? &world : parentObj;
+						obj->deserialize(copyInfo);
+						pobj->addChild(*obj);
+						select(obj, info.gui);
+					}
+				}
+				copyInfo.name = oldName;
+			}
+		}
+	}
+}
+
+void EditorWindow::traverse(Object & obj, GUI& gui)
+{
+	for (auto b = obj.children.begin(), e = obj.children.end(); b != e; b++) {
+		Object& obj = **b;
+		ImGuiTreeNodeFlags node_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick;
+		if (obj.children.empty())
+			node_flags |= ImGuiTreeNodeFlags_Leaf;
+		if (selectedObj == &obj)
+			node_flags |= ImGuiTreeNodeFlags_Selected;
+		bool isOpen = ImGui::TreeNodeEx(obj.name.c_str(), node_flags);
+		if (ImGui::IsItemClicked()) {
+			select(&obj, gui);
+		}
+		if (isOpen) {
+			traverse(obj, gui);
+			ImGui::TreePop();
+		}
+	}
+}
+
+void EditorWindow::select(Object * obj, GUI& gui)
+{
+	selectedObj = obj;
+	InspectorWindow* ipw = dynamic_cast<InspectorWindow*>(gui.getUIControl("Inspector"));
+	if (ipw == NULL) {
+		ipw = new InspectorWindow(world, "Inspector", true);
+		world += ipw;
+
+	}
+	ipw->setTargetObject(*obj);
+	ipw->show = true;
 }
--- Tangram/EditorWindow.h
+++ Tangram/EditorWindow.h
@@ -8,10 +8,18 @@ class EditorWindow : public UIWindow
 {
 public:
 	Material& baseMat;
+	Object* selectedObj = NULL;
 	bool alwaysShow = false;
+	float mainVolume = 1;
+	Object* parentObj = NULL;
+	SerializationInfo copyInfo;
 
 	EditorWindow(Object& object, Material& baseMat, string name = "Editor", bool defaultShow = false);
 	virtual void onRenderWindow(GUIRenderInfo& info);
+	virtual void onPostAction(GUIPostInfo& info);
+protected:
+	void traverse(Object& obj, GUI& gui);
+	void select(Object* obj, GUI& gui);
 };
 
 #endif // !_EDITORWINDOW_H_
