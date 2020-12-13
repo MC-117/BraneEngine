b100b5780d14f5ab4f1abf342d508f902f25afe1
MC-117 <295566948@qq.com>
2020-12-13 21:16:02 +0800

    Complete: spring arm

--- Tangram/SpringArm.cpp
+++ Tangram/SpringArm.cpp
@@ -1,9 +1,11 @@
 #include "SpringArm.h"
 #include "Engine.h"
 
-SpringArm::SpringArm(const string & name) : handle("Handle")
+SerializeInstance(SpringArm);
+
+SpringArm::SpringArm(const string & name) : ::Transform(name), handle("Handle")
 {
-	addChild(handle);
+	addInternalNode(handle);
 }
 
 float SpringArm::getSpringLength()
@@ -16,6 +18,16 @@ void SpringArm::setSpringLength(float len)
 	springLength = len;
 }
 
+float SpringArm::getRadius()
+{
+	return radius;
+}
+
+void SpringArm::setRadius(float radius)
+{
+	this->radius = radius;
+}
+
 void SpringArm::tick(float deltaTime)
 {
 	::Transform::tick(deltaTime);
@@ -23,7 +35,7 @@ void SpringArm::tick(float deltaTime)
 	World* world = Engine::getCurrentWorld();
 	if (world == NULL)
 		return;
-	if (world->physicalWorld.physicsScene->sweep(PxSphereGeometry(2), getWorldTransform(),
+	if (world->physicalWorld.physicsScene->sweep(PxSphereGeometry(radius), getWorldTransform(),
 		toPVec3(-getForward(WORLD)), springLength, hit, PxHitFlag::eDEFAULT, PxQueryFilterData())) {
 		if (hit.block.distance > 0)
 			handle.setPosition(-hit.block.distance, 0, 0);
@@ -33,4 +45,29 @@ void SpringArm::tick(float deltaTime)
 	else {
 		handle.setPosition(-springLength, 0, 0);
 	}
+}
+
+void SpringArm::addChild(Object & child)
+{
+	handle.addChild(child);
+}
+
+Serializable * SpringArm::instantiate(const SerializationInfo & from)
+{
+	SpringArm* sa = new SpringArm(from.name);
+	ChildrenInstantiateObject(from, sa);
+	return sa;
+}
+
+bool SpringArm::deserialize(const SerializationInfo & from)
+{
+	return ::Transform::deserialize(from);
+}
+
+bool SpringArm::serialize(SerializationInfo & to)
+{
+	if (!::Transform::serialize(to))
+		return false;
+	to.type = "SpringArm";
+	return true;
 }
--- Tangram/SpringArm.h
+++ Tangram/SpringArm.h
@@ -7,16 +7,26 @@
 class SpringArm : public ::Transform
 {
 public:
+	Serialize(SpringArm);
+
 	::Transform handle;
 
 	SpringArm(const string& name = "SpringArm");
 
 	virtual float getSpringLength();
 	virtual void setSpringLength(float len);
+	virtual float getRadius();
+	virtual void setRadius(float radius);
 
 	virtual void tick(float deltaTime);
+
+	virtual void addChild(Object& child);
+
+	static Serializable* instantiate(const SerializationInfo& from);
+	virtual bool deserialize(const SerializationInfo& from);
+	virtual bool serialize(SerializationInfo& to);
 protected:
-	float springLength = 100;
+	float springLength = 50;
 	float radius = 2;
 };
 
